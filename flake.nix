dafc94695c10401c9760084158b2a991ffbb0b8c
Celibistrial <ryan80222@gmail.com>
2024-03-15 16:02:31 +0530

    Refactored flake.nix

--- flake.nix
+++ flake.nix
@@ -3,41 +3,35 @@
     nixpkgs.url = github:nixOS/nixpkgs/release-23.05;
     utils.url = github:numtide/flake-utils;
   };
-  outputs = { nixpkgs, utils, ... }:
-    utils.lib.eachSystem [ "i686-linux" "x86_64-linux" ]
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
-        in
-        rec {
-          packages =
-            let
-              buildFlags = [ "PREFIX=$(out)" "confdir=/etc" ];
-            in
-            rec {
-              nbfc = pkgs.stdenv.mkDerivation {
-                inherit buildFlags;
+  outputs = {
+    nixpkgs,
+    utils,
+    ...
+  }:
+    utils.lib.eachSystem ["i686-linux" "x86_64-linux"]
+    (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      buildFlags = ["PREFIX=$(out)" "confdir=/etc"];
+      installFlags = ["PREFIX=$out" "confdir=$out/etc"];
+    in rec {
+      packages = rec {
+        nbfc = pkgs.stdenv.mkDerivation {
+          inherit buildFlags;
 
-                name = "nbfc-linux";
-                version = "0.1.6";
+          name = "nbfc-linux";
+          version = "0.1.15";
 
-                src = nixpkgs.lib.cleanSource ./.;
+          src = nixpkgs.lib.cleanSource ./.;
 
-                installPhase =
-                  let
-                    installFlags = [ "PREFIX=$out" "confdir=$out/etc"];
-                  in
-                  ''
-                    make ${builtins.concatStringsSep " " installFlags}\
-                         install-core \
-                         install-configs\
-                         install-docs\
-                         install-completion
-                  '';
-              };
-            };
-          defaultPackage = packages.nbfc;
-        });
+          installPhase = ''
+            make ${builtins.concatStringsSep " " installFlags}\
+                 install-core \
+                 install-configs\
+                 install-docs\
+                 install-completion
+          '';
+        };
+        default = packages.nbfc;
+      };
+    });
 }
